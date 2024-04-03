fb34e45047daf29fbc3d121f0b289fa7da3b2e5a
AmirrezaAP <AmirrezaAPTech@gmail.com>
2024-04-03 20:39:32 +0330

    Create About Page

---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='flex justify-center items-center min-h-screen bg-[#F7F8F0]'>
+    <div className='w-[58%] h-[38vh] rounded-full border-[#8d9676] p-5 border-solid border-[1px] flex justify-center items-center overflow-hidden'>
+        <div className='bg-[#CBD9BC] w-[100%] h-full rounded-full flex flex-col items-center justify-start p-10 font-serif'>
+        <h1 className='text-white text-4xl font-semibold'>ABOUT TEA & GO</h1>
+        <p className='text-white text-center text-xl mt-auto'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil error dolores tenetur illum cumque optio impedit vel, enim magni voluptate incidunt, in beatae tempora. Pariatur dolore quam eum autem vero!Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        </p>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default About
