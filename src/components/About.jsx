28613bc21b866798eda7022bee7fa064d0d2572a
AmirrezaAP <AmirrezaAPTech@gmail.com>
2024-04-03 23:04:55 +0330

    Make About Page Responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const About = () => {
   return (
     <div className='flex justify-center items-center min-h-screen bg-[#F7F8F0]'>
-    <div className='w-[58%] h-[38vh] rounded-full border-[#8d9676] p-5 border-solid border-[1px] flex justify-center items-center overflow-hidden'>
-        <div className='bg-[#CBD9BC] w-[100%] h-full rounded-full flex flex-col items-center justify-start p-10 font-serif'>
-        <h1 className='text-white text-4xl font-semibold'>ABOUT TEA & GO</h1>
-        <p className='text-white text-center text-xl mt-auto'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil error dolores tenetur illum cumque optio impedit vel, enim magni voluptate incidunt, in beatae tempora. Pariatur dolore quam eum autem vero!Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+    <div className='w-[58%] max-w-[1113px] max-2xl:w-[70%] max-xl:w-[85%] max-lg:w-[90%] max-sm:w-full h-[360px] max-xl:h-[320px] max-md:h-[260px]  max-sm:h-[220px] rounded-full border-[#8d9676] p-5 border-solid border-[1px] flex justify-center items-center overflow-hidden'>
+        <div className='bg-[#CBD9BC] w-[100%] h-full rounded-full flex flex-col items-center justify-start p-10 max-md:p-5 font-serif'>
+        <h1 className='text-white text-4xl font-semibold max-md:text-3xl max-sm:text-xl'>ABOUT TEA & GO</h1>
+        <p className='text-white text-center text-xl mt-auto max-md:text-sm max-sm:text-xs'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil error dolores tenetur illum cumque optio impedit vel, enim magni voluptate incidunt, in beatae tempora. Pariatur dolore quam eum autem vero!Lorem ipsum, dolor sit amet consectetur adipisicing elit.
         </p>
         </div>
     </div>
