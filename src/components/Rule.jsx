b0e436ddad98d61ae292993c7c2e595bc2673062
AmirrezaAP <AmirrezaAPTech@gmail.com>
2024-04-03 23:15:47 +0330

    Added Rule Section

---
+++ src/components/Rule.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Rule = () => {
+  return (
+    <div className='w-screen min-h-screen flex justify-center items-center'>
+    <div className='flex justify-center items-center w-[85%] mx-auto gap-5'>
+        <div className='flex flex-col justify-between items-right w-[50%] h-[300px] text-right gap '>
+            <h1 className='text-4xl text-[#5A6D48]'>RULES OF MAHJANG</h1>
+            <p className='text-lg text-[#A2A29F] border-r-[2px] border-r-[#5A6D48] pr-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi tempore ducimus voluptates corporis numquam magni at quos velit officia, minus, sequi laborum voluptatem dolorem et amet excepturi! Quod, amet tempore.
+            Ab saepe dolores, quisquam provident tenetur assumenda dicta, atque blanditiis reiciendis autem eius voluptates exercitationem optio cupiditate, dignissimos ut! Distinctio alias libero doloribus fuga fugit perspiciatis expedita vero voluptas quidem!</p>
+        </div>
+        <div className='bg-gray-600 w  w-[650px] h-[350px] rounded-3xl'></div>
+    </div>
+    </div>
+  )
+}
+
+export default Rule
