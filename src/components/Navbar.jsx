f785a6b5d814ce813689afe6e5a52adcfe170136
fardinzam <fardinzaman51@gmail.com>
2024-04-05 12:28:26 -0700

    created navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,14 @@
+// import Logo from "../../public/Logo"
+
 const Navbar = () => {
   return (
-    <div>
-      <p>Navbar</p>
+    <div className="w-screen bg-green-100 flex flex-row justify-between">
+      <div className="h-12 w-12 mx-5 bg-gray-200"></div>
+      <div className="flex flex-row mx-5 my-auto">
+        <p className="mx-2">Team</p>
+        <p className="mx-2">Events</p>
+        <p className="mx-2">Photos</p>
+      </div>
     </div>
   );
 };
