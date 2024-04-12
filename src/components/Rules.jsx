377a15c5c7847a8207960d8c27597ddd5059dd60
AxelPere <Paxel1001@gmail.com>
2024-04-12 12:19:57 -0700

    Updated name, title text size, and made border gray

--- src/components/Rules.jsx
+++ src/components/Rules.jsx
@@ -4,8 +4,8 @@ const Rules = () => {
   return (
     <div className="flex">
       <div className="flex flex-col pt-5">
-        <h1 className="text-tag-green-200 font-serif text-lg text-right pr-10">
-          RULES OF MAHJANG
+        <h1 className="text-tag-green-200 font-serif text-xl text-right pr-10">
+          RULES OF MAHJONG
         </h1>
         <div className="flex pt-3">
           <p className="text-tag-gray-100 font-serif text-xs text-right pl-72 mr-3 flex-1 leading-loose">
@@ -14,11 +14,11 @@ const Rules = () => {
             AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT
             ALIQUIP EX EA COMMODO CONSEQUAT.
           </p>
-          <div className="border-l border-black h-full pr-10"></div>
+          <div className="border-l border-tag-gray-100 h-full pr-10"></div>
         </div>
       </div>
       <div className="flex pr-64">
-        <Image src={board} alt="Mahjang_Board" width={900} height={100} />
+        <Image src={board} alt="Mahjong_Board" width={900} height={100} />
       </div>
     </div>
   );
