ff297936276c6a5f6bd3554f5959ed4a7ca66909
adidi-c <mradidi18@gmail.com>
2024-04-14 18:25:44 -0700

    added rules section for Go

--- src/components/Rules.jsx
+++ src/components/Rules.jsx
@@ -1,7 +1,10 @@
+import Box from "@/components/Box";
+
 const Rules = () => {
   return (
-    <div>
-      <p>Rules</p>
+    <div className="w-full flex flex-col justify-center">
+      <p className="text-3xl text-tag-green-200">OUR GAMES</p>
+      <Box />
     </div>
   );
 };
