1765f8d621cb737d691e911755e8c66ad0c1f7ba
raulrazo <raulrazox@gmail.com>
2024-04-19 12:35:05 -0700

    Ran npm run format

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
 const Button = () => {
   return (
-    <div>
-      <p>Button</p>
-    </div>
+    <Link href="https://google.com" target="_blank" rel="noopener noreferrer">
+      <button className="items-center rounded-full bg-tan-100 border-solid px-8 py-2 text-2xl border border-green-800 hover:bg-green-800 hover:text-white transition-colors ease-in duration-150">
+        <p>JOIN NOW</p>
+      </button>
+    </Link>
   );
 };
 
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,7 +1,22 @@
+import Logo from "../../public/Logo.webp";
+import Button from "@/components/Button";
+import Image from "next/image";
 const Title = () => {
   return (
-    <div>
-      <p>Title</p>
+    <div className="flex flex-col justify-center items-center">
+      <div>
+        <div className="flex flex-col mb-4 mr-48 items-center">
+          <Image src={Logo} alt="Logo" className="w-1/4 py-2 ml-14"></Image>
+          <p className="text-6xl">Tea & Go</p>
+        </div>
+        <div className="flex flex-col text-right w-3/5">
+          <div className="text-3xl">Come join us on wednesdays for a</div>
+          <div className="text-3xl">fun night of TEA!</div>
+        </div>
+        <div className="flex justify-center mr-40 mt-16 ">
+          <Button />
+        </div>
+      </div>
     </div>
   );
 };
