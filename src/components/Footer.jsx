c1db81faebb638466512f4afd3e20531d7436f1a
Andrey Felix <146979092+ElAndreu@users.noreply.github.com>
2024-04-19 04:29:16 +0000

    Addressed comments

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,38 +1,31 @@
-// import Image from "next/image";
-// import Logo from "@/public/Logo.webp";
+import Image from "next/image";
+import Logo from "@/public/Logo.webp";
 import { BsDiscord } from "react-icons/bs"; //  for discord
 import { BsInstagram } from "react-icons/bs"; //  for insta
 import { BsEnvelope } from "react-icons/bs"; // for mailing
 
 const Footer = () => {
   return (
-    <div className="box-content h-32 w-128 bg-gradient-to-r from-lime-950 via-lime-900 to-lime-800">
-      <p className="text-gray-100 absolute bottom-[-16%] left-[5%]">Tea & Go</p>
-
-      <div className="absolute bottom-[-19%] left-[1%]">
-        <img
-          className="h-16 max-w-xl rounded-lg shadow-xl dark:shadow-gray-800"
-          src="Logo.webp"
-        ></img>
+    <div className="flex w-full items-end justify-between bg-gradient-to-r from-tag-green-300 to-tag-green-200">
+      <div className="flex m-3 items-center">
+        <Image className="h-16 w-16" src={Logo} />
+        <p className="text-3xl text-gray-100">Tea & Go</p>
       </div>
 
-      <div className="absolute bottom-[-15%] left-[75%] text-gray-100">
-        <p>Contact Us</p>
-        <p>900 University Ave</p>
-        <p>Riverside CA, 92521</p>
-      </div>
+      <div className="flex m-10">
+        <div className="text-lg text-gray-100">
+          <p>Contact Us</p>
+          <p>900 University Ave</p>
+          <p>Riverside CA, 92521</p>
+        </div>
 
-      {/* <div className="box-content absolute bottom-[10] bg">
-
-        </div> */}
-
-      <div className="bg-opacity-25 flex justify-center items-center">
-        <div className="absolute bottom-[-16%] left-[86%]">
-          <div className="bg-white w-2.5 h-20 shadow"></div>
-
-          <BsDiscord className=" absolute bottom-[45%] left-[200%] fill-gray-100" />
-          <BsInstagram className="absolute bottom-[79%] left-[200%] fill-gray-100" />
-          <BsEnvelope className="absolute bottom-[10%] left-[200%] fill-gray-100" />
+        <div className="flex">
+          <div className="bg-white w-2.5 h-20 mx-3"></div>
+          <div className="flex flex-col justify-between text-2xl text-gray-100">
+            <BsDiscord />
+            <BsEnvelope />
+            <BsInstagram />
+          </div>
         </div>
       </div>
     </div>
