ac5d0b21b34a2de4cd970aeca968658597b9ffbb
Ari Lee <arielleharyanto@gmail.com>
2024-04-19 12:19:34 -0700

    update on header2

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,12 +3,12 @@ import Caligraphy from "../../public/Caligraphy.webp";
 
 const Title = ({ text }) => {
   return (
-    <div className="relative w-full h-[230px] rounded-b-[100%] bg-tag-green-200">
+    <div className="relative w-full h-[230px] rounded-b-[40%] bg-tag-green-200">
       <div className="w-full">
         <Image
           src={Caligraphy}
           alt="Caligraphy"
-          className="object-cover object-bottom h-[230px] w-full rounded-b-[100%] opacity-60"
+          className="object-cover object-center h-[230px] w-full rounded-b-[40%] opacity-60"
         />
         <div className="bottom-1/4 absolute font-serif text-5xl text-white w-full text-center">
           {text}
