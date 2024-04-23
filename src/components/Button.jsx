54e13e3aeffb2559edb7eef59d6054784a334149
fardinzam <fardinzaman51@gmail.com>
2024-04-22 17:21:42 -0700

    added button prop

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
-const Button = () => {
+const Button = ({ text }) => {
   return (
     <Link href="https://google.com" target="_blank" rel="noopener noreferrer">
       <button className="items-center rounded-full bg-tan-100 border-solid px-8 py-2 text-2xl border border-green-800 hover:bg-green-800 hover:text-white transition-colors ease-in duration-150">
-        <p>JOIN NOW</p>
+        <p>{text}</p>
       </button>
     </Link>
   );
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -6,7 +6,7 @@ const Title = () => {
     <div className="flex flex-col justify-center items-center">
       <div>
         <div className="flex flex-col mb-4 mr-48 items-center">
-          <Image src={Logo} alt="Logo" className="w-1/4 py-2 ml-14"></Image>
+          <Image src={Logo} alt="Logo" className="w-1/4 py-2 ml-14" />
           <p className="text-6xl">Tea & Go</p>
         </div>
         <div className="flex flex-col text-right w-3/5">
@@ -14,7 +14,7 @@ const Title = () => {
           <div className="text-3xl">fun night of TEA!</div>
         </div>
         <div className="flex justify-center mr-40 mt-16 ">
-          <Button />
+          <Button text="JOIN NOW" />
         </div>
       </div>
     </div>
