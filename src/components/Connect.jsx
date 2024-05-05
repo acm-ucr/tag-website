45f708f9cd1f9f7718af8b63e6f22a5a01282644
AxelPere <Paxel1001@gmail.com>
2024-05-05 14:19:42 -0700

    Added links to the connect bubbles

--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -3,6 +3,7 @@ import { FaInstagram } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { IoLogoFacebook } from "react-icons/io";
 import { FiLink } from "react-icons/fi";
+import Link from "next/link";
 
 const Connect = () => {
   return (
@@ -10,19 +11,44 @@ const Connect = () => {
       <p>Get Connected </p>
       <div className="w-4/5 flex justify-center text-5xl text-tag-green-200 justify-between">
         <div className="border-[1px] border-tag-green-200 rounded-full m-3 p-3">
-          <FaInstagram />
+          <Link
+              target="_blank"
+              href="https://www.instagram.com/ucr_tag/"
+          >
+            <FaInstagram />
+          </Link>
         </div>
         <div className="border-[1px] border-tag-green-200 rounded-full m-3 p-3">
-          <FaDiscord />
+          <Link
+              target="_blank"
+              href="https://discord.com/invite/peSZUJWhvS"
+          >
+            <FaDiscord />
+          </Link>
         </div>
         <div className="border-[1px] border-tag-green-200 rounded-full m-3 p-3">
-          <MdEmail />
+          <Link
+              target="_blank"
+              href="/"
+          >
+            <MdEmail />
+          </Link>
         </div>
         <div className="border-[1px] border-tag-green-200 rounded-full m-3 p-3">
-          <IoLogoFacebook />
+          <Link
+              target="_blank"
+              href="/"
+          >
+            <IoLogoFacebook />
+          </Link>
         </div>
         <div className="border-[1px] border-tag-green-200 rounded-full m-3 p-3">
-          <FiLink />
+          <Link
+              target="_blank"
+              href="/"
+          >
+            <FiLink />
+          </Link>
         </div>
       </div>
     </div>
