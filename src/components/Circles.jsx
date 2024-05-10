76994f02da2593a3470f7011d177b5ee2daedc31
randall-nguyen <rnguy137@ucr.edu>
2024-05-09 20:56:19 -0700

    implemented responsiveness hero section

--- src/components/Circles.jsx
+++ src/components/Circles.jsx
@@ -5,11 +5,11 @@ import Go from "@/public/Go.webp";
 const Circles = () => {
   return (
     <div className="relative h-300 w-40">
-      <div className="absolute bottom-[16%] left-[90%] w-64 h-64 overflow-hidden rounded-full z-10">
+      <div className="absolute top-[44%] md:bottom-[16%] left-[76%] md:left-[90%] w-36 h-36 md:w-64 md:h-64 overflow-hidden rounded-full z-10">
         <Image src={Tea} alt="Tea" className="object-cover w-full h-full" />
       </div>
 
-      <div className="absolute -left-8 top-[20%] w-64 h-64 overflow-hidden rounded-full">
+      <div className="absolute left-[14%] md:-left-8 top-[23%] md:top-[20%] w-36 h-36 md:w-64 md:h-64 overflow-hidden rounded-full">
         <Image
           src={Mahjong}
           alt="Mahjong"
@@ -17,11 +17,11 @@ const Circles = () => {
         />
       </div>
 
-      <div className="absolute left-[170%] top-[10%] w-64 h-64 overflow-hidden rounded-full">
+      <div className="absolute left-[126%] md:left-[170%] top-[14.5%] md:top-[10%] w-36 h-36 md:w-64 md:h-64 overflow-hidden rounded-full">
         <Image src={Go} alt="Go" className="object-cover w-full h-full" />
       </div>
 
-      <div className="absolute left-[150%] top-[5%] w-80 h-80 overflow-hidden rounded-full border-2 border-tag-green-100"></div>
+      <div className="absolute left-[116%] md:left-[150%] top-[10%] md:top-[5%] w-44 h-44 md:w-80 md:h-80 overflow-hidden rounded-full border-2 border-tag-green-100"></div>
     </div>
   );
 };
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,19 +4,19 @@ import Image from "next/image";
 
 const Title = () => {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <div>
-        <div className="flex flex-col mb-4 mr-48 items-center">
-          <Image src={Logo} alt="Logo" className="w-1/4 py-2 ml-14" />
-          <p className="text-6xl">Tea & Go</p>
+    <div>
+      <div className="flex flex-col mb-4 md:mr-48 items-center">
+        <Image src={Logo} alt="Logo" className="w-1/3 md:w-1/4 py-2 md:ml-14" />
+        <p className="text-4xl md:text-6xl">Tea & Go</p>
+      </div>
+      <div className="flex flex-col items-center md:text-right md:w-4/5">
+        <div className="text-xl md:text-3xl">
+          Come join us on wednesdays for a
         </div>
-        <div className="flex flex-col text-right w-3/5">
-          <div className="text-3xl">Come join us on wednesdays for a</div>
-          <div className="text-3xl">fun night of TEA!</div>
-        </div>
-        <div className="flex justify-center mr-40 mt-16 ">
-          <Button text="JOIN NOW" />
-        </div>
+        <div className="text-xl md:text-3xl">fun night of TEA!</div>
+      </div>
+      <div className="flex justify-center md:mr-40 mt-16 ">
+        <Button text="JOIN NOW" />
       </div>
     </div>
   );
