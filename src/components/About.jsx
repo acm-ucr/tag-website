08ea345fdb71c269797f4007c4e2e6d942c4d353
Ari Lee <arielleharyanto@gmail.com>
2024-05-10 11:29:53 -0700

    about responsiveness

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
       <div className="border-[1px] w-4/5 h-[300px] border-tag-green-100 rounded-[500px] flex justify-center items-center">
         <div className="bg-[url('/Mahjong.webp')] bg-cover w-[95%] h-[87%] rounded-[500px] flex flex-col justify-center items-center text-white">
           <div className="from-tag-green-200/80 bg-gradient-to-l to-black/80 bg-[url('/Mahjong.webp')] bg-cover w-[100%] h-[100%] rounded-[500px] flex flex-col justify-center items-center text-white">
-            <p className="sm:text-xl lg:text-3xl md:text-2xl  my-3">
+            <p className="lg:text-3xl md:text-2xl sm:text-xl my-3">
               ABOUT TEA & GO
             </p>
             <p className="my-5 lg:text-xl md:text-md sm:text-sm text-center w-4/5">
