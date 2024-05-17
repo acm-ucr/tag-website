2f51757d4759b495d0bb12aae11a1e291bfac52d
Andrey Felix <146979092+ElAndreu@users.noreply.github.com>
2024-05-17 05:27:31 +0000

    Adding responsiveness

--- src/components/MahjongRules.jsx
+++ src/components/MahjongRules.jsx
@@ -3,11 +3,13 @@ import board from "../../public/MahjongPieces.webp";
 
 const MahjongRules = () => {
   return (
-    <div className="flex justify-center items-center">
-      <div className="w-[35%] text-right">
-        <p className="text-tag-green-200 text-2xl mx-5">Rules of Mahjong</p>
-        <div className="flex pt-3">
-          <p className="text-tag-gray-100 text-xs leading-loose">
+    <div className="flex justify-center items-center flex-col">
+      <div className="mb-10 w-[50%] md:w-[20%]text-right text-6xl">
+        <p className="text-tag-green-200 text-2xl lg:text-4xl">
+          Rules of Mahjong
+        </p>
+        <div className="flex w-[120%] md:w-[120%] ">
+          <p className="text-tag-gray-100 text-xs leading-loose lg:text-xl">
             LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO
             EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE MAGNA ALIQUA. UT ENIM
             AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT
@@ -16,7 +18,11 @@ const MahjongRules = () => {
           <div className="w-2 h-auto border-l bg-tag-gray-100 mx-5"></div>
         </div>
       </div>
-      <Image src={board} alt="Mahjong_Board" className="w-[30%] mx-5" />
+      <Image
+        src={board}
+        alt="Mahjong_Board"
+        className="w-[45%] md:[25%] lg:w-[15%]mx-5"
+      />
     </div>
   );
 };
