a6c1705a014017e5e5e00e9d415a5bf105a0d88b
Sarbesh1234 <sankarsarbesh@gmail.com>
2024-05-17 13:02:26 -0700

    Tried Making Connect Section Responsiveness

--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -7,10 +7,10 @@ import Link from "next/link";
 
 const Connect = () => {
   return (
-    <div className="w-1/2 my-10 mx-auto flex flex-col items-center text-2xl text-tag-green-200">
+    <div className="w-1/2 my-10 mx-auto flex flex-col justify-center items-center text-2xl text-tag-green-200">
       <p>Get Connected </p>
-      <div className="w-4/5 flex justify-center text-5xl text-tag-green-200 justify-between">
-        <div className="border-[1px] border-tag-green-200 rounded-full m-3 p-3">
+      <div className="w-4/5 flex justify-between text-5xl text-tag-green-200">
+        <div className="border-[1px] border-tag-green-200 rounded-full sm:w-20 sm:h-20 md:w-20 md:h-20 lg:w-20 lg:h-20 flex justify-center align-items: center">
           <Link target="_blank" href="https://www.instagram.com/ucr_tag/">
             <FaInstagram />
           </Link>
