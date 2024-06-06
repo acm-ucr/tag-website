a9b9d1c2f4728d8a23357ee788e269cd8cd45b4c
Ari Lee <arielleharyanto@gmail.com>
2024-06-06 11:10:02 -0700

    Header Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import TeaHeader from "../../public/TeaHeader.png";
 const Header = ({ text }) => {
   return (
     <div className="relative bottom-56 w-full z-[-1]">
-      <Image src={TeaHeader} alt="Header" className="w-full" />
+      <Image src={TeaHeader} alt="Tea Header" className="w-full" />
       <div className="absolute -mt-40 font-serif text-5xl text-white w-full text-center">
         {text}
       </div>
