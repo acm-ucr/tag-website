9e6e782a0b17441b38171d4c8920d63a92997e46
raulrazo <raulrazox@gmail.com>
2024-06-07 12:28:12 -0700

    fixes

--- src/components/Circles.jsx
+++ src/components/Circles.jsx
@@ -18,7 +18,7 @@ const Circles = () => {
         />
       </div>
 
-      <div className=" lg:left-72 md:left-52 sm:left-40 lg:top-0 md:top-0 sm:top-4 absolute overflow-hidden rounded-full border-2 border-tag-green-100 lg:w-80 lg:h-80 md:w-64 md:h-64 sm:w-48 sm:h-48 flex items-center justify-center">
+      <div className=" lg:left-72 md:left-52 sm:left-44 lg:top-0 md:top-0 sm:top-4 absolute overflow-hidden rounded-full border-2 border-tag-green-100 lg:w-80 lg:h-80 md:w-60 md:h-60 sm:w-52 sm:h-52 flex items-center justify-center">
         <div className="overflow-hidden rounded-full lg:w-64 lg:h-64 md:w-48 md:h-48 sm:w-40 sm:h-40">
           <Image src={Go} alt="Go" className="object-cover w-full h-full" />
         </div>
