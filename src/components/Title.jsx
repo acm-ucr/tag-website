a8495bc4f588756138527fe21e43561a9279bfdd
raulrazo <raulrazox@gmail.com>
2024-06-07 10:32:58 -0700

    recreated circles + responsiveness

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -9,11 +9,11 @@ const Title = () => {
         <Image src={Logo} alt="Logo" className="w-1/3 xl:w-1/4 py-2 xl:ml-14" />
         <p className="text-4xl md:text-5xl xl:text-6xl">Tea & Go</p>
       </div>
-      <div className="flex flex-col items-center md:text-right xl:w-4/5">
-        <div className="text-xl md:text-3xl">
+      <div className="flex flex-col items-center xl:w-4/5">
+        <div className="text-xl md:text-2xl">
           Come join us on wednesdays for a
         </div>
-        <div className="text-xl md:text-3xl">fun night of TEA!</div>
+        <div className="text-xl md:text-2xl">fun night of TEA!</div>
       </div>
       <div className="flex justify-center xl:mr-40 mt-16 ">
         <Button text="JOIN NOW" />
