52fb16b855d8fa30b6698792fbcc4cef8a45c681
fardinzam <fardinzaman51@gmail.com>
2024-07-03 14:00:50 -0700

    home page fixes

--- src/components/Home/Title.jsx
+++ src/components/Home/Title.jsx
@@ -4,17 +4,13 @@ import Image from "next/image";
 
 const Title = () => {
   return (
-    <div>
-      <div className="flex flex-col mb-4 xl:mr-48 items-center">
-        <Image src={Logo} alt="Logo" className="w-1/3 xl:w-1/4 py-2 xl:ml-14" />
+    <div className="flex flex-col items-center mr-10">
+      <div className="flex flex-col items-center">
+        <Image src={Logo} alt="logo" className="w-1/3 xl:w-1/4" />
         <p className="text-4xl md:text-5xl xl:text-6xl">Tea & Go</p>
-      </div>
-      <div className="flex flex-col items-center xl:w-4/5">
-        <p className="text-xl md:text-2xl w-3/5 text-right">
+        <p className="text-md md:text-2xl text-right my-3">
           Come join us on wednesdays for a fun night of TEA!
         </p>
-      </div>
-      <div className="flex justify-center xl:mr-40 mt-16 ">
         <Button
           text="JOIN NOW"
           link="https://discord.com/invite/peSZUJWhvS"
